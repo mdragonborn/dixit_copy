8eb117228213fd5ff5ce77548ba890ba3e1709e8
Konstantin Đorđević <vomindoraan@gmail.com>
2018-06-05 20:24:05 +0200

    Add dixit to INSTALLED_APPS

--- dixit/settings.py
+++ dixit/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'profiles.apps.ProfilesConfig',
+    'dixit',
+    'profiles',
 ]
 
 MIDDLEWARE = [
