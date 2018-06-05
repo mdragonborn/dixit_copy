b29c85f08dd651091f08b92e10b228584eac814f
mdragonborn <lena.markovic996@gmail.com>
2018-06-05 18:56:13 +0200

    MyHand refactor

--- templates/components/game/MyHand.jsx
+++ templates/components/game/MyHand.jsx
@@ -3,38 +3,35 @@ import {HandGrid} from './styles';
 
 export default class MyHand extends React.Component {
   render() {
-    console.log(this.props.cards);
-    return (
-      <HandGrid>
-        { this.props.gameStatus === "PICK_CARD" ?
-            this.props.cards.map(
+    let content;
+    switch(this.props.gameStatus){
+      case "PICK_CARD":
+        content =  this.props.cards.map(
             i => <img src={i.image}
                         onClick={() => this.props.onClickPickedCard(i.id)}
                         alt={i.codename}
                    />
-          ):
-          this.props.gameStatus === "WAITING_FOR_OTHERS"
-          || this.props.gameStatus ==="PICK_FROM_TABLE"
-            || this.props.gameStatus === "WAITING_FOR_RESULTS"
-            ?
-            this.props.cards.map(
+          );
+        break;
+      case "WAITING_FOR_OTHERS":
+      case "PICK_FROM_TABLE":
+      case "WAITING_FOR_RESULTS":
+        content = this.props.cards.map(
               i => <img src={ i.id===this.props.currentlyPicked?this.props.defaultCard:i.image }
                         alt={'default'}
                    />
-            )
-            :
-          this.props.cards.length === 0 ?
-            Array(6).fill(1).map(
-              i => <img src = {this.props.defaultCard}
-                        alt="default"
-              />
-            ):
-            this.props.cards.map(
+            );
+        break;
+      default:
+        content = this.props.cards.map(
               i => <img src={i.image}
                         alt={i.codename}
                    />
-            )
-        }
+            );
+    }
+    return (
+      <HandGrid>
+        {content}
       </HandGrid>)
   }
 }
