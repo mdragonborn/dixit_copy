4f5cab8f56667149f12fba6ee678d052e81ef4c8
Konstantin Đorđević <konstantin.djordjevic@tradecore.com>
2018-06-08 17:38:25 +0200

    Fix tartaljatrain2.jpg

--- dixit/settings.py
+++ dixit/settings.py
@@ -149,11 +149,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
-]
-TEST_DIRECT_STATIC = os.path.join(BASE_DIR, 'static')
 
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+TEST_DIRECT_STATIC = os.path.join(BASE_DIR, 'static')
 
 WEBPACK_LOADER = {
     'DEFAULT' : {
