0dd9b409ffd3d088066fc82a513ced9d2884a6a4
Konstantin Đorđević <konstantin.djordjevic@tradecore.com>
2018-06-13 17:48:31 +0200

    Format webpack.config.js so editorconfig doesn't complain

--- webpack.config.js
+++ webpack.config.js
@@ -1,39 +1,35 @@
-var path = require("path");
-var webpack = require('webpack');
-var BundleTracker = require('webpack-bundle-tracker');
+const path = require('path');
+const webpack = require('webpack');
+const BundleTracker = require('webpack-bundle-tracker');
 
 module.exports = {
-    context: __dirname,
-    entry: {
-        game: './game/templates/components/game/index'
-    },
-  output: {
-      path: path.resolve('./static/bundles/'),
-      filename: "[name]-[hash].js"
+  context: __dirname,
+  entry: {
+    game: './game/templates/components/game/index',
   },
-
-    plugins: [
-        new webpack.HotModuleReplacementPlugin(),
-        new webpack.NoEmitOnErrorsPlugin(), // don't reload if there is an error
-        new BundleTracker({path: __dirname, filename: './webpack-stats.json'})
+  output: {
+    path: path.resolve('./static/bundles/'),
+    filename: '[name]-[hash].js',
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoEmitOnErrorsPlugin(), // don't reload if there is an error
+    new BundleTracker({path: __dirname, filename: './webpack-stats.json'}),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.jsx$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['es2015', 'react'],
+        },
+      },
     ],
-
-    module: {
-        rules: [
-            {
-                test: /\.jsx$/,
-                exclude: /(node_modules|bower_components)/,
-                loader: 'babel-loader',
-                query: {
-                    presets: ['es2015', 'react']
-                }
-            }
-
-        ]
-    },
-
-    resolve: {
-        modules: ['node_modules', 'bower_components'],
-        extensions: ['.js', '.jsx']
-    }
+  },
+  resolve: {
+    modules: ['node_modules', 'bower_components'],
+    extensions: ['.js', '.jsx'],
+  },
 };
